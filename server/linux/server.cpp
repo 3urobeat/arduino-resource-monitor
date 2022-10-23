da68063f441cdd4a1d80ba63993951ba4a4f1255
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2022-10-23 11:57:50 +0200

    Remove usage of deprecated egrep

--- server/linux/server.cpp
+++ server/linux/server.cpp
@@ -4,7 +4,7 @@
  * Created Date: 04.02.2022 20:47:18
  * Author: 3urobeat
  * 
- * Last Modified: 10.02.2022 11:22:26
+ * Last Modified: 23.10.2022 11:42:34
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
@@ -109,7 +109,7 @@ void intervalEvent() {
 
     //Get CPU stats
     char cpuLoad[8] = "";
-    getStdoutFromCommand(cpuLoad, "mpstat 1 1 | egrep -v '^Linux|^$' | awk -v c=\"%idle\" 'NR==1 {for (i=1; i<=NF; i++) if ($i==c) break}''{print $(i-1)}' | tail -1 | awk '{print 100-$1}'"); //using this command for cutting the response was easier for now than doing it here - Credit: https://www.linuxquestions.org/questions/linux-newbie-8/need-to-get-average-idle-time-using-mpstat-4175545709/ (I added the subtraction)
+    getStdoutFromCommand(cpuLoad, "mpstat 1 1 | grep -E -v '^Linux|^$' | awk -v c=\"%idle\" 'NR==1 {for (i=1; i<=NF; i++) if ($i==c) break}''{print $(i-1)}' | tail -1 | awk '{print 100-$1}'"); //using this command for cutting the response was easier for now than doing it here - Credit: https://www.linuxquestions.org/questions/linux-newbie-8/need-to-get-average-idle-time-using-mpstat-4175545709/ (I added the subtraction)
     gcvt(round(atof(cpuLoad)), 3, cpuLoad); //convert to float, floor, restrict digits to max 3 and convert float back to char arr
     strcat(cpuLoad, "%");
 
