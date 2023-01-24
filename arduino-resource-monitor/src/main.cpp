2d02abcb51840327602771ea92d26eaefdc84344
3urobeat <35304405+HerrEurobeat@users.noreply.github.com>
2023-01-24 17:43:28 +0100

    Change License to GPLv3

--- arduino-resource-monitor/src/main.cpp
+++ arduino-resource-monitor/src/main.cpp
@@ -4,14 +4,14 @@
  * Created Date: 04.02.2022 21:12:30
  * Author: 3urobeat
  * 
- * Last Modified: 24.01.2023 15:54:19
+ * Last Modified: 24.01.2023 17:41:37
  * Modified By: 3urobeat
  * 
  * Copyright (c) 2022 3urobeat <https://github.com/HerrEurobeat>
  * 
- * Licensed under the MIT license: https://opensource.org/licenses/MIT
- * Permission is granted to use, copy, modify, and redistribute the work.
- * Full license information available in the project LICENSE file. 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
  */
 
 
