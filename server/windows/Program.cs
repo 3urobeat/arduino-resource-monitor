ae420399a5ec217234d527e4f6ab74dcf6ddeeda
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-11-12 12:21:40 +0100

    Init Windows server

---
+++ server/windows/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * File: Program.cs
+ * Project: arduino-resource-monitor
+ * Created Date: 12.11.2023 11:34:19
+ * Author: 3urobeat
+ *
+ * Last Modified: 12.11.2023 12:20:53
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+Console.WriteLine("Hello World");
